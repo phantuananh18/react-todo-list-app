1db6afc23ff0585ae1498154816fa7fa59c0d75b
phantuananh18 <tuananh.61779@gmail.com>
2022-04-19 16:22:01 +0700

    delete and complete todo

---
+++ src/redux/reducers/TodoReducer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { v4 } from 'uuid';
+
+const initialState = {
+    todoList: [],
+    isCompleted: false,
+}
+
+const TodoReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return state;
+        case 'DELETE_TODO':
+            return state;
+        case 'COMPLETE_TODO':
+            return state;
+        default:
+            return state;
+    }
+}
+
+export default TodoReducer;
