fb58692d5903ddf53eb85ec1b6f4da89cbd6ba67
phantuananh18 <tuananh.61779@gmail.com>
2022-04-19 17:53:30 +0700

    add-redux

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,34 +1,51 @@
+// import React from 'react';
+// import { Input, Button } from '@mui/material';
+// import TodoList from './TodoList';
+// import '../App.css';
+
+// const TodoForm = ({ onAddTodo, onChangeTextInput, onDeleteTodo, onCompleteTodo, todoList, textInput }) => {
+
+//     return (
+//         <div className='todo-form'>
+//             <Input
+//                 autoFocus
+//                 placeholder='Write a new task...'
+//                 value={textInput}
+//                 onChange={onChangeTextInput}
+//             />
+//             <Button
+//                 variant='contained'
+//                 color='success'
+//                 style={{
+//                     marginLeft: 10,
+//                 }}
+//                 onClick={onAddTodo}
+//                 disabled={!textInput}
+//             >
+//                 Add a new task
+//             </Button>
+//             <div>
+//                 <TodoList todoList={todoList} onDeleteTodo={onDeleteTodo} onCompleteTodo={onCompleteTodo} />
+//             </div>
+//         </div >
+//     )
+// }
+
+// export default TodoForm;
+
+/** apply redux */
 import React from 'react';
-import { Input, Button } from '@mui/material';
+import { useSelector } from 'react-redux';
 import TodoList from './TodoList';
 
-import '../App.css'
-
-const TodoForm = ({ onAddTodo, onChangeTextInput, onDeleteTodo, onCompleteTodo, todoList, textInput }) => {
-
+const TodoForm = () => {
+    const todoList = useSelector(state => state.todo.isCompleted);
+    console.log('TodoLists: ', todoList);
     return (
-        <div className='todo-form'>
-            <Input
-                autoFocus
-                placeholder='Write a new task...'
-                value={textInput}
-                onChange={onChangeTextInput}
-            />
-            <Button
-                variant='contained'
-                color='success'
-                style={{
-                    marginLeft: 10,
-                }}
-                onClick={onAddTodo}
-                disabled={!textInput}
-            >
-                Add a new task
-            </Button>
-            <div>
-                <TodoList todoList={todoList} onDeleteTodo={onDeleteTodo} onCompleteTodo={onCompleteTodo} />
-            </div>
-        </div >
+        <div>
+            <h1>Refractor redux todo list app</h1>
+            <TodoList></TodoList>
+        </div>
     )
 }
 
