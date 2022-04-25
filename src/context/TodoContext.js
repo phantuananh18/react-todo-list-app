f36cecea3ccdc6d5251938766c297a5835b833a2
phantuananh18 <tuananh.61779@gmail.com>
2022-04-25 14:10:22 +0700

    update

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -37,9 +37,7 @@ export const TodoContextProvider = (props) => {
 
     const value = {
         textInput,
-        setTextInput,
         todoList,
-        setTodoList,
         inputRef,
         onChangeTextInput,
         onAddTodo,
